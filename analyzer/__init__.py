19de044849d8386a18343ae16b357fe16d2dbc1b
liuwei9438 <595048689@qq.com>
2026-07-22 18:15:24 +0800

    Delete analyzer directory

--- analyzer/__init__.py
+++
@@ -1,9 +0,0 @@
-from .product_profile_schema import ProductProfile, empty_profile
-from .fact_lock import build_fact_lock, validate_fact_lock
-from .profile_validator import normalize_profile, validate_profile
-from .product_understanding import ProductUnderstandingEngine, UnderstandingError
-
-__all__ = [
-    "ProductProfile", "empty_profile", "build_fact_lock", "validate_fact_lock",
-    "normalize_profile", "validate_profile", "ProductUnderstandingEngine", "UnderstandingError",
-]
